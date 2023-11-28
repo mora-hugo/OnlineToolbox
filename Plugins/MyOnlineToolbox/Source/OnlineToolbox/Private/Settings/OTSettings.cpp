49aa8dca8427fbf29aadd356277dca336d77a5ae
Mora Hugo <100521440+mora-hugo@users.noreply.github.com>
2023-11-27 23:22:25 -0500

    Delete Plugins/MyOnlineToolbox directory

--- Plugins/MyOnlineToolbox/Source/OnlineToolbox/Private/Settings/OTSettings.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Settings/OTSettings.h"
-
-UOTSettings::UOTSettings(const FObjectInitializer& obj)
-{
-
-}
